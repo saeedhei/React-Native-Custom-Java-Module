bec25c0ed7f44fa111e383f7218e0e5c6732ecc6
Daniel Zuzevich <danielzuzevich@gmail.com>
2017-05-29 19:29:21 -0400

    Update AlarmReceiver.java

--- AlarmReceiver.java
+++ AlarmReceiver.java
@@ -1,15 +1,9 @@
-package com.wakeupify;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
-
-/**
- * Created by z on 5/28/2017.
- */
 
 public class AlarmReceiver extends BroadcastReceiver {
 
